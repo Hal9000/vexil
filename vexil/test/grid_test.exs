f70de396766f29601cb28a2bbfcd3f226a9fb7f1
rubygeek <nola@rubygeek.com>
2017-08-27 18:41:03 -0500

    separte modules to own files, added some tests

--- vexil/test/grid_test.exs
+++ vexil/test/grid_test.exs
@@ -8,4 +8,12 @@ defmodule GridTest do
     grid = Grid.put(grid, where, val)
     assert Grid.get(grid, where) == val
   end
+
+  test "show cell" do
+    grid = Grid.put(%{}, {:red, 1, 1}, "piece")    
+    assert Grid.show_cell(grid, 1, 2) == :ok
+  end
+
+
+
 end
---
+++ vexil/test/referee_test.exs
@@ -0,0 +1,6 @@
+ExUnit.start()
+
+defmodule RefereeTest do
+  use ExUnit.Case
+
+end
